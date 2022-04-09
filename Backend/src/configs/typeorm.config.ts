1fe68e8b4215c10338d66f461fc0a25ce94efc75
Ianara-cs <ianarasilva333@gmail.com>
2022-04-09 16:01:59 -0300

    Ajustando configuração do typeorm

--- Backend/src/configs/typeorm.config.ts
+++ Backend/src/configs/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'nara',
-  database: 'academic-class',
+export const typeOrmConfig = {
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
-};
+} as TypeOrmModuleOptions;
