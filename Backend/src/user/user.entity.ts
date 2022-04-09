d3c1a21282b90f3b188aa30136ecd8fd4c607e39
Ianara-cs <ianarasilva333@gmail.com>
2022-04-09 17:31:12 -0300

    primeira versão da entidade user

---
+++ Backend/src/user/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  email: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
