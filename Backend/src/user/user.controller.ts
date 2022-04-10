1bf52ac5ee8fd5d42fb525239257d31a9016b29e
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2022-04-10 15:53:52 -0300

    implementando o CRUD (função delete não funciona)

--- Backend/src/user/user.controller.ts
+++ Backend/src/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { UserService } from './shared/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+    
+    @Get()
+    async index(){
+      return await this.userService.findALL();
+    } 
+
+    @Post()
+    async create(@Body() body){
+        return await this.userService.create(body);
+    }
+
+    @Get(':id')
+    async show(@Param('id', new ParseUUIDPipe()) id: string){
+        return await this.userService.findOne(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body){
+        return await this.userService.update(id, body);
+    }
+    
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async destroy(@Param('id', new ParseUUIDPipe()) id: string){
+        await this.userService.deleteById(id);
+    }
+}
